c79dc7687aa19d5c8e3db2861afae90ff3d42fa8
happi89 <farhaanp99@gmail.com>
2022-10-03 22:55:37 -0400

    fixed res values

--- server/main.py
+++ server/main.py
@@ -22,34 +22,34 @@ app.add_middleware(
 def calculate_student(name, mark, courseCode):
     aboveAchievment = ' You are above provincial standard'
     belowAchievment = ' You are below provincial standard'
-    returnStatment = 'name:', name, '\n', 'course code:', courseCode, '\n','mark:', mark
+    returnStatment = {"name": name, 'course code': courseCode,'mark': mark}
 
     if(mark >= 95 and mark <= 100):
-        return returnStatment, '\n', aboveAchievment, ' and your grade is 4+'
+        return returnStatment, aboveAchievment, ' and your grade is 4+'
     elif(mark >= 87 and mark <= 94):
-        return returnStatment, '\n', aboveAchievment, ' and your grade is 4'
+        return returnStatment, aboveAchievment, ' and your grade is 4'
     elif(mark >= 80 and mark <= 96):
-        return returnStatment, '\n', aboveAchievment, ' and your grade is 4-'
+        return returnStatment, aboveAchievment, ' and your grade is 4-'
     elif(mark >= 77 and mark <= 79):
-        return returnStatment, '\n', aboveAchievment, ' and your grade is 3+'
+        return returnStatment, aboveAchievment, ' and your grade is 3+'
     elif(mark >= 73 and mark <= 76):
-        return returnStatment, '\n', aboveAchievment, ' and your grade is 3'
+        return returnStatment, aboveAchievment, ' and your grade is 3'
     elif(mark >= 70 and mark <= 72):
-        return returnStatment, '\n', aboveAchievment, ' and your grade is 3-'
+        return returnStatment, aboveAchievment, ' and your grade is 3-'
     elif(mark >= 67 and mark <= 69):
-        return returnStatment, '\n', belowAchievment, ' and your grade is 2+'
+        return returnStatment, belowAchievment, ' and your grade is 2+'
     elif(mark >= 63 and mark <= 66):
-        return returnStatment, '\n', belowAchievment, ' and your grade is 2'
+        return returnStatment, belowAchievment, ' and your grade is 2'
     elif(mark >= 60 and mark <= 62):
-        return returnStatment, '\n', belowAchievment, ' and your grade is 2-'
+        return returnStatment, belowAchievment, ' and your grade is 2-'
     elif(mark >= 50 and mark <= 59):
-        return returnStatment, '\n', belowAchievment, ' and your grade is 1'
+        return returnStatment, belowAchievment, ' and your grade is 1'
     elif(mark < 50):
-        return returnStatment, '\n', belowAchievment, ' and your grade is 0'
+        return returnStatment, belowAchievment, ' and your grade is 0'
     else:
         return 'invalid input'
 
 @app.get("/")
-async def root(name: string, mark: number, courseCode: string):
+async def calculate_grade(name: str, mark: int, courseCode: str):
     result = calculate_student(name, mark, courseCode)
     return { "result": result }
