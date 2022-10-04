b31565ce9b19d509f99844ac7c0c089b22d9bf8e
happi89 <farhaanp99@gmail.com>
2022-10-04 14:22:36 -0400

    fixed cors

--- server/main.py
+++ server/main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "https://grading-system-python-hw.vercel.app",
+    "https://grading-system-python-hw.vercel.app/?",
     "http://localhost:3000",
     "http://localhost:3001",
     "http://localhost:3001/?",
